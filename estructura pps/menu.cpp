eebdfaea11198ce0f12542bf12f658f8bc5f1831
trenchoman <83569665+trenchoman@users.noreply.github.com>
2022-04-28 13:53:22 -0300

    Update menu.cpp

--- estructura pps/menu.cpp
+++ estructura pps/menu.cpp
@@ -87,9 +87,80 @@ void mostrarValores(){
     
   }//switch end
 
-  
+  ///////////////////////////////////////////////////////FUNCIONES//////////////////////////////////////////////////////////////////
+
+//Funciones:::::
+void selectOption(){
+  if(digitalRead(sw) == LOW){
+     lcd.clear(); 
+      menu.call_function(1);
+      delay(500);
+  }
+}
+
+ 
   void fn_atras(){
   menu.change_screen(1);
   menu.set_focusedLine(0);
   page_counter=0;
 }
+
+
+void fn_led1(){
+   
+  page_counter = 1;
+  Valores();
+  lcd.clear();  
+  menu.change_screen(2);
+  menu.set_focusedLine(0);
+}
+void fn_led2(){
+  menu.change_screen(3);
+  menu.set_focusedLine(0);
+}
+*/
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+void mostrarValores(){
+ /*switch (page_counter) {
+   
+    case 1:{     //Design of home page 1
+      
+      lcd.setCursor(0,0);
+      lcd.print("Isal:"); lcd.print(corriente,0); lcd.print("A");
+   
+      lcd.setCursor(9,0);
+        
+      lcd.print("Pd:");
+      if (potenciad<=200)
+      {
+      lcd.print(potenciad,0); 
+      lcd.print("W");
+      }
+      else
+      lcd.print("200W"); 
+      
+      lcd.setCursor(0,1);
+      lcd.print("Vex:"); lcd.print(vex,0); lcd.print("V");
+      
+      lcd.setCursor(9,1);
+      lcd.print("AG:"); lcd.print(agc,1); lcd.print("V");
+    }
+    break;
+    case 2: { //Design of page 2 
+     lcd.setCursor(0,0);
+     lcd.print("This is");
+     lcd.setCursor(0,1);
+     lcd.print("Page 2");
+    }
+    break;
+    case 3: {   //Design of page 3 
+     lcd.setCursor(0,0);
+     lcd.print("You are now on");
+     lcd.setCursor(0,1);
+     lcd.print("Page 3");
+    }
+    break;
+    
+  }//switch end
+  
+ 
