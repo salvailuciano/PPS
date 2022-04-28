c6bf6423647a6f38333566b79abcc7d69413232e
salvai <40271174+salvai@users.noreply.github.com>
2022-04-28 13:47:09 -0300

    Update promediador.cpp

--- estructura pps/promediador.cpp
+++ estructura pps/promediador.cpp
@@ -1,1 +1,11 @@
+//Promediador
 
+for(j=0;j<MUESTRAS;j++)
+    {
+    valoresPromedio = analogRead(0); //Leemos el pin analógico Ai  
+    acumprom+= valoresPromedio ;
+    }
+    valoresPromedio = acumprom / MUESTRAS;
+    acumprom = 0;
+    return valoresPromedio;
+  }
