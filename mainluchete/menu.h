4074412b72a21bd387b39d946c6e536a8dbc8a41
Lucho <salvailuciano@gmail.com>
2022-06-20 15:46:56 -0300

    mainluchete, nose q onda, pero para no borrarlo

---
+++ mainluchete/menu.h
@@ -0,0 +1,10 @@
+
+
+void leerBotones();
+void setup_menu();
+void leerPagina(int pagina);
+void mostrarValores(float a,float b,float c,float d);
+//void selectOption();
+void fn_mediciones();
+void fn_configuracion();
+void fn_mas_configuraciones();
