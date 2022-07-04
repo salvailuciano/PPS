76b0f478bc90806516976ca3a48c4ff768770273
Lucho <salvailuciano@gmail.com>
2022-07-04 17:21:15 -0300

    Cambio grande en el main, quedo funcionando probado en pantalla serial de arduino, se borro practicamente lo de readyprom, y se crearon funciones adentro del main que despues hay que ver q hacer. Ahora toma 10 muestras y las guarda en un arreglo en la primer funcion, en la segunda funcion saca el promedio de las 10 muestras y retorna el valor promedio que luego es enviado para imprimir en el serial

--- main/main.ino
+++ main/main.ino
@@ -7,7 +7,7 @@ link del repositorio de github: https://github.com/salvailuciano/PPS
 
 MEDICIONES:
 1.  PD: potencia transferida
-2.  PR: potencia reglejada
+2.  PR: potencia reflejada
 3.  AGC:
 4.  I salida: (8,8 A)
 5.  V salida: (48 v)
@@ -37,24 +37,26 @@ desactiva el pin de interrupcion
 
 //Variables Utiles//
 const int cantidadMediciones = 4;
+const int cantidadMuestras = 10;
 const float nivelesDigitalesADC = 1023.0;
 const float vRefADC = 5;
 
 //Variables auxiliares para calculo de ADC//
-const float Iparametro  = 0.1;
-//const float Pdparametro  = 1033.05;//(v*v/100)*pdparametro
-const float Pdparametro  = 1250;
-const float Vexparametro  = 0.1;
-const float Agcparametro  = 1;
-
-//Variables a mostrar en pantalla//
-float corriente = 0.0;
-float potenciad = 0.0;
-float vex = 0.0;
-float agc = 0.0;
+const float parametroPD = 1;
+//const float Pdparametro = 1033.05;//(v*v/100)*pdparametro
+const float parametroPR = 2;
+const float parametroAGC = 10;
+const float parametroIsal = 100;
+const float parametroVsal = 1;
+const float parametroVexc = 1;
+const float parametroVaux = 1;
+const float parametroVlinea = 1;
 
 #define inInterrupcion_2 2 // Se define pin 2 para interrupcion
 #define inInterrupcion_3 3 // Se define pin 3 para interrupcion
+
+float tomarMedicion(int medicionN, float parametro, float arreglo[]);
+float calcularProm(float arreglo[]);
 
 ////////////////////////////////////////////////////////////SETUP//////////////////////////////////////////////////////////////////
 void setup(){
@@ -66,43 +68,131 @@ void setup(){
   attachInterrupt(digitalPinToInterrupt(inInterrupcion_2), temperatura, RISING);// deja activa la interrupcion 
   attachInterrupt(digitalPinToInterrupt(inInterrupcion_3), roe, RISING);// deja activa la interrupcion 
 }
+
 ////////////////////////////////////////////////////////////LOOP//////////////////////////////////////////////////////////////////
 void loop(){
-  float aux = 0.0;
-  float lectura[cantidadMediciones];
+//Se declaran todos los arreglos de cada medicion de 10 muestras cada 1
+float potenciaTransferida[cantidadMuestras] {};
+float potenciaReflejada[cantidadMuestras] {};
+float AGC[cantidadMuestras] {};
+float corrienteSalida[cantidadMuestras] {};
+float tensionSalida[cantidadMuestras] {};
+float tensionExc[cantidadMuestras] {};
+float tensionAux[cantidadMuestras] {};
+float tensionLinea[cantidadMuestras] {};
 
 //////////////////////LECTURA DE ENTRADA////////////////////////////
-// "i=0" Contador importante, por que es el que indica el valor adentro del arreglo
-  for(int i=0; i < cantidadMediciones; i++){
-    selectChannelMux(i); //Selecciona el canal del multiplexor para tomar la entrada a promediar
-    //lectura[i] = readyProm();  //Toma (lee entrada) las MUESTRAS y carga el valor ya promediado en lectura[i]
-    lectura[i] = analogRead(i); // Solo para test sin promediador
-  }
-
-/////////////////////////////ARREGLO DE LECTURAS/////////////////////////////
-// lectura = [     0     ,     1     ,     2     ,     3     ,     4     ] //
-// lectura = [ corriente , potenciad ,    vex    ,    agc    ,     X     ] //
-/////////////////////////////////////////////////////////////////////////////
-
-// Se podria pasar todo esto de los calculos a una sheet nueva que sea "CalculosADC" o algo por el estilo/////
-  Serial.println("calculando");
-  corriente = ((lectura[0]*vRefADC)/nivelesDigitalesADC)/Iparametro;
- // corriente = (lectura[0]*5) / 1024;// Solo para test
-
-  //potenciad = ((lectura[1]*vRefADC)/nivelesDigitalesADC)/Pdparametro; // ver este que es distinto
-  aux = (lectura[1] * 5.0) / 1024.0;
-  aux = (aux*aux)/100;
-  potenciad = aux *Pdparametro;
- // potenciad = (lectura[1]*5) / 1024;// Solo para test sin el parametro
- 
-  vex = ((lectura[2]*vRefADC)/nivelesDigitalesADC)/Vexparametro;
- // vex = (lectura[2]*5) / 1024;// Solo para test sin el parametro
+  Serial.print("Mediciones 1: ");
+  float potenciaTransferidaProm = tomarMedicion(0, parametroPD, potenciaTransferida);
+  Serial.print("Mediciones 2: ");
+  float potenciaReflejadaProm = tomarMedicion(1, parametroPR, potenciaReflejada);
+  Serial.print("Mediciones 3: ");
+  float AGCProm = tomarMedicion(2, parametroAGC, AGC);
+  Serial.print("Mediciones 4: ");
+  float corrienteSalidaProm = tomarMedicion(3, parametroIsal, corrienteSalida);
+  float tensionSalidaProm = tomarMedicion(4, parametroVsal, tensionSalida);
+  float tensionExcProm = tomarMedicion(5, parametroVexc, tensionExc);
+  float tensionAuxProm = tomarMedicion(6, parametroVaux, tensionAux);
+  float tensionLineaProm = tomarMedicion(7, parametroVlinea, tensionLinea);
   
-  agc = ((lectura[3]*vRefADC)/nivelesDigitalesADC)/Agcparametro;
- // agc = (lectura[3]*5) / 1024; // Solo para test sin el parametro
+  mostrarValores(potenciaTransferidaProm, potenciaReflejadaProm, AGCProm, corrienteSalidaProm);
   
-///////////////////////Funciones para el menu y los botones///////////////////////////////////
-  leerBotones();
-  mostrarValores(corriente,potenciad,vex,agc);
+  Serial.print("PD promedio: ");
+  Serial.println(potenciaTransferidaProm);
+  Serial.print("PR promedio: ");
+  Serial.println(potenciaReflejadaProm);
+  Serial.print("AGC promedio: ");
+  Serial.println(AGCProm);
+  Serial.print("Isal promedio: ");
+  Serial.println(corrienteSalidaProm);
+/*
+  for(int medicion = 0; medicion < cantidadMediciones; medicion++){
+    selectChannelMux(medicion); //Selecciona el canal del multiplexor para tomar la entrada a medir
+      switch(medicion){
+        case 0:                           
+              for(int muestraActual = 0 ; muestraActual < cantidadMuestras ; muestraActual++){
+                potenciaTransferida[muestraActual] = ((analogRead(0)*vRefADC)/nivelesDigitalesADC)/parametroPD;
+              }
+              //for(int muestraActual = 0 ; muestraActual < cantidadMuestras ; muestraActual++){
+              //  Serial.println(F("Initialize System"));
+              //}
+              //readyProm();
+              break; 
+        case 1:
+              for(int muestraActual = 0 ; muestraActual < cantidadMuestras ; muestraActual++){
+                potenciaReflejada[muestraActual] = ((analogRead(0)*vRefADC)/nivelesDigitalesADC)/parametroPR;
+              }
+              //readyProm();
+              break;
+        case 2:
+              for(int muestraActual = 0 ; muestraActual < cantidadMuestras ; muestraActual++){
+                AGC[muestraActual] = ((analogRead(0)*vRefADC)/nivelesDigitalesADC)/parametroAGC;
+              }
+              //readyProm();
+              break;
+        case 3:
+              for(int muestraActual = 0 ; muestraActual < cantidadMuestras ; muestraActual++){
+                corrienteSalida[muestraActual] = ((analogRead(0)*vRefADC)/nivelesDigitalesADC)/parametroIsal;
+              }
+              //readyProm();
+              break;*/
+/*      case 4:
+              for(int muestraActual = 0 ; muestraActual < cantidadMuestras ; muestraActual++){
+                tensionSalida[muestraActual] = ((analogRead(0)*vRefADC)/nivelesDigitalesADC)/parametroVsal;
+              }
+              //readyProm();
+              break;
+        case 5:
+              for(int muestraActual = 0 ; muestraActual < cantidadMuestras ; muestraActual++){
+                tensionExc[muestraActual] = ((analogRead(0)*vRefADC)/nivelesDigitalesADC)/parametroVexc;
+              }
+              //readyProm();
+              break; 
+        case 6:
+              for(int muestraActual = 0 ; muestraActual < cantidadMuestras ; muestraActual++){
+                tensionAux[muestraActual] = ((analogRead(0)*vRefADC)/nivelesDigitalesADC)/parametroVaux;
+              }
+              //readyProm();
+              break;
+        case 7:
+              for(int muestraActual = 0 ; muestraActual < cantidadMuestras ; muestraActual++){
+                tensionLinea[muestraActual] = ((analogRead(0)*vRefADC)/nivelesDigitalesADC)/parametroVlinea;
+              }
+              //readyProm();
+              break; */
+//    }  
+//potenciad = ((lectura[1]*vRefADC)/nivelesDigitalesADC)/Pdparametro; // ver este que es distinto
+//aux = (lectura[1] * 5.0) / 1024.0;
+//aux = (aux*aux)/100;
+//potenciad = aux *Pdparametro;
+//potenciad = (lectura[1]*5) / 1024;// Solo para test sin el parametro
 }
 /////////////////////////////////////////////////////////TERMINA EL LOOP//////////////////////////////////////////////////////////////
+
+
+//////////////////////FUNCION tomarMedicion////////////////////////////
+float tomarMedicion(int medicionN, float parametro, float arreglo[]){
+  selectChannelMux(medicionN);
+  for(int muestraActual = 0 ; muestraActual < cantidadMuestras ; muestraActual++){
+    arreglo[muestraActual] = ((analogRead(0)*vRefADC)/nivelesDigitalesADC)/parametro;
+  }
+  for(int muestraActual = 0 ; muestraActual < cantidadMuestras ; muestraActual++){
+    Serial.print(arreglo[muestraActual]);
+    Serial.print(" ");
+  }
+  Serial.println();
+  leerBotones();
+  return calcularProm(arreglo);
+}
+//////////////////////FUNCION calcularProm////////////////////////////
+float calcularProm(float arreglo[]){
+  float valorPromedio;
+  float acumProm=0;
+  
+  for(int muestraActual = 0 ; muestraActual < cantidadMuestras ; muestraActual++){
+    acumProm += arreglo[muestraActual];
+  }
+  valorPromedio = acumProm / cantidadMuestras;
+  acumProm  = 0;
+  return valorPromedio;//Retorna  el valor  promedio  de la cantidad  de muestras
+}
--- main/promediador.cpp
+++ main/promediador.cpp
@@ -1,21 +1,2 @@
 #include <Arduino.h>
-#define MUESTRAS 100 //Cantidad de muestras a promediar
-
-///////////////////////////////PROMEDIADOR Y LEE ENTRADAS/////////////////////////////////////
-//El canal ya fue seleccionado en selectChannelMux(i);, entonces por ese canal se toman las muestras a promediar
-
-float readyProm(){
-  float valorPromedio;
-  float acumProm=0;
-  int j=0;
-  
-  for(j=0;j<MUESTRAS;j++){
-    valorPromedio = analogRead(0); //Leemos el pin analógico Ai
-    acumProm += valorPromedio ;
-  }
-  valorPromedio = acumProm / MUESTRAS;
-  acumProm = 0;
-  return valorPromedio; //Retorna el valor promedio de la cantidad de muestras
-}
-
   
